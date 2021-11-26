426e524c5f027191f967ee9e76c02db01e01fcc4
Nuno Costa <91nunocosta@gmail.com>
2021-11-26 00:31:26 +0000

    feat: add check task

--- literature/tasks.py
+++ literature/tasks.py
@@ -1,9 +1,18 @@
 """Provides tasks."""
-from invoke import Collection
+from invoke import Collection, task
 
 from literature import code, tox
 
+
+@task
+def check(c):
+    """Check implementation."""
+    code.check_source(c)
+    tox.run(c)
+
+
 ns = Collection(
+    check,
     code,
     tox,
 )
